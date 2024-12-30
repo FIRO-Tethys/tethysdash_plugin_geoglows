71b89fbdd76065b1877069b75e2a2048d8e43848
Corey Krewson <ckrewson@aquaveo.com>
2024-12-30 10:29:13 -0600

    set variable inputs working

--- geoglows_visualizations/frontend/src/App.js
+++ geoglows_visualizations/frontend/src/App.js
@@ -1,23 +1,25 @@
-import React, { useEffect} from 'react';
-import 'ol/ol.css';
-import {
-  Map
-} from 'backlayer';
-import MapComponentContent from './MapContent';
+import React, { useEffect } from "react";
+import "ol/ol.css";
+import { Map } from "backlayer";
+import MapComponentContent from "./MapContent";
 
-const MapComponent = (
-  { 
-    mapConfig, 
-    viewConfig, 
-    layers,
-    legend
-  }) => {
-
+const MapComponent = ({
+  mapConfig,
+  viewConfig,
+  layers,
+  legend,
+  setVariableInputValues,
+}) => {
   return (
-    <Map {...mapConfig} >
-      <MapComponentContent viewConfig={viewConfig} layers={layers} legend={legend} />
+    <Map {...mapConfig}>
+      <MapComponentContent
+        viewConfig={viewConfig}
+        layers={layers}
+        legend={legend}
+        setVariableInputValues={setVariableInputValues}
+      />
     </Map>
   );
-}
+};
 
 export default MapComponent;

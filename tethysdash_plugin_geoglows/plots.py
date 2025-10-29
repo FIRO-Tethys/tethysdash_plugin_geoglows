52167d0fa52079eac1f9fd3e63d535b70c6d5c50
Sonia-96 <ysun@aquaveo.com>
2025-10-29 11:23:33 -0600

    add bias correct historical plots

--- tethysdash_plugin_geoglows/plots.py
+++ tethysdash_plugin_geoglows/plots.py
@@ -1,11 +1,13 @@
 from intake.source import base
 import geoglows
+import pandas as pd
 import numpy as np
 from .utilities import (
     get_plot_data, plot_retro_simulation, plot_retro_annual_status, plot_yearly_volumes, plot_retro_fdc,
     flood_probabilities, plot_ssi_each_month_since_year, plot_ssi_one_month_each_year
 )
 from datetime import datetime
+import json
 
 
 class Plots(base.DataSource):
@@ -31,8 +33,12 @@ class Plots(base.DataSource):
             {"value": "forecast-stats", "label": "Forecast Statistics"},
             {"value": "forecast-ensembles", "label": "Forecast Ensembles"},
             {"value": "retro-simulation", "label": "Retrospective Simulation"},
+            {"value": "retro-simulation-bias-corrected", "label": "Retrospective Simulation (Bias Corrected)"},
+            {"value": "bias-performance", "label": "Bias Correction Performance"},
             {"value": "retro-daily", "label": "Retrospective Daily Averages"},
+            {"value": "retro-daily-bias-corrected", "label": "Retrospective Daily Averages (Bias Corrected)"},
             {"value": "retro-monthly", "label": "Retrospective Monthly Averages"},
+            {"value": "retro-monthly-bias-corrected", "label": "Retrospective Monthly Averages (Bias Corrected)"},
             {"value": "retro-yearly", "label": "Retrospective Yearly Averages"},
             {"value": "retro-yearly-volume", "label": "Yearly Cumulative Discharge Volume"},
             {"value": "retro-status", "label": "Annual Status by Month"},  # need Year
@@ -43,6 +49,7 @@ class Plots(base.DataSource):
         ],
         "month": [{"value": i, "label": i} for i in range(1, 13)],
         "year": [{"value": i, "label": i} for i in range(1940, datetime.now().year)],
+        "observed_historical_data": "csv-uploader"
     }
     visualization_group = "GEOGLOWS"
     visualization_label = "GEOGLOWS Plots"
@@ -50,38 +57,61 @@ class Plots(base.DataSource):
     visualization_attribution = 'pygeoglows'
     _user_parameters = []
 
-    def __init__(self, river_id, plot_name, year, month, metadata=None):
+    def __init__(self, river_id, plot_name, year, month, observed_historical_data=None, metadata=None):
         self.river_id = int(river_id)
         self.plot_name = plot_name
         self.year = year
         self.month = month
+        self.observed_historical_data = observed_historical_data
         super(Plots, self).__init__(metadata=metadata)
 
     def read(self):
-        df_return = get_plot_data(self.river_id, "return-periods")
+        df_observed = None
+        if self.observed_historical_data and 'bias' in self.plot_name:
+            try:
+                dict_bias = json.loads(self.observed_historical_data)
+            except json.JSONDecodeError as e:
+                raise ValueError("Uploaded observed historical data is not valid JSON:", e[:100], "...")
+            df_observed = pd.DataFrame(dict_bias).replace("", np.nan)\
+                .assign(Datetime=lambda x: x['Datetime'].astype('datetime64[ns]')).set_index('Datetime')
+            df_observed["Streamflow (m3/s)"] = df_observed["Streamflow (m3/s)"].astype(float)
+            df_observed.index = df_observed.index.tz_localize("UTC")
+            df_retro_daily = get_plot_data(self.river_id, "retro-daily")
+            # TODO check if it's historical or forecast data
+            df_corrected = geoglows.bias.correct_historical(df_retro_daily, df_observed)
+
+        df_rp = get_plot_data(self.river_id, "return-periods")
 
         match self.plot_name:
             case "forecast":
                 df = get_plot_data(self.river_id, self.plot_name)
-                plot = geoglows.plots.forecast(df, rp_df=df_return)
+                plot = geoglows.plots.forecast(df, rp_df=df_rp)
             case "forecast-stats":
                 df = get_plot_data(self.river_id, self.plot_name)
-                plot = geoglows.plots.forecast_stats(df, rp_df=df_return)
+                plot = geoglows.plots.forecast_stats(df, rp_df=df_rp)
             case "forecast-ensembles":
                 df = get_plot_data(self.river_id, self.plot_name)
-                plot = geoglows.plots.forecast_ensembles(df, rp_df=df_return)
+                plot = geoglows.plots.forecast_ensembles(df, rp_df=df_rp)
             case "retro-simulation":
                 df_retro_daily = get_plot_data(self.river_id, "retro-daily")
                 df_retro_monthly = get_plot_data(self.river_id, "retro-monthly")
                 plot = plot_retro_simulation(df_retro_daily, df_retro_monthly, self.river_id)
+            case "retro-simulation-bias-corrected":
+                plot = geoglows.plots.corrected_retrospective(df_corrected, df_retro_daily, df_observed, df_rp)
+            case "bias-performance":
+                plot = geoglows.plots.corrected_scatterplots(df_corrected, df_retro_daily, df_observed)
             case "retro-daily":
                 df = get_plot_data(self.river_id, self.plot_name)
                 plot = geoglows.plots.daily_averages(df)
+            case "retro-daily-bias-corrected":
+                plot = geoglows.plots.corrected_day_average(df_corrected, df_retro_daily, df_observed)
             case "retro-monthly":
                 df = get_plot_data(self.river_id, self.plot_name)
                 df['month'] = df.index.strftime('%m')
                 df = df.groupby('month').mean()
                 plot = geoglows.plots.monthly_averages(df)
+            case "retro-monthly-bias-corrected":
+                plot = geoglows.plots.corrected_month_average(df_corrected, df_retro_daily, df_observed)
             case "retro-yearly":
                 df = get_plot_data(self.river_id, self.plot_name)
                 plot = geoglows.plots.annual_averages(df)
@@ -97,23 +127,12 @@ class Plots(base.DataSource):
                 plot = plot_retro_fdc(df_retro_daily, self.river_id)
             case "exceedance":
                 df_ensemble = get_plot_data(self.river_id, "forecast-ensembles")
-                df_return = get_plot_data(self.river_id, "return-periods")
-                plot = flood_probabilities(df_ensemble, df_return)
+                df_rp = get_plot_data(self.river_id, "return-periods")
+                plot = flood_probabilities(df_ensemble, df_rp)
             case "ssi-monthly":
                 plot = plot_ssi_each_month_since_year(
                     self.river_id, 2010
                 )  # TODO year is hardcoded?
             case "ssi-one-month":
                 plot = plot_ssi_one_month_each_year(self.river_id, self.month)
-
-        data = []
-        for trace in plot.data:
-            trace_json = trace.to_plotly_json()
-            if "x" in trace_json and isinstance(trace_json["x"], np.ndarray):
-                trace_json["x"] = trace_json["x"].tolist()
-            if "y" in trace_json and isinstance(trace_json["y"], np.ndarray):
-                trace_json["y"] = trace_json["y"].tolist()
-            data.append(trace_json)
-        layout = plot.to_plotly_json()["layout"]
-        config = {"autosizable": True, "responsive": True}
-        return {"data": data, "layout": layout, "config": config}
+        return json.loads(plot.to_json())
